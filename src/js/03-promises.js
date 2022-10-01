4e2b87bc1139de71e8f3e3aaa7b961987e0a14ec
Светлана Петровна Андрела <104157616+ASvitlana@users.noreply.github.com>
2022-10-01 12:05:24 +0300

    Task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
+import Notiflix from 'notiflix';
+
+const delay = document.querySelector('input[name="delay"]');
+const step = document.querySelector('input[name="step"]');
+const amount = document.querySelector('input[name="amount"]');
+const btnCreatePromise = document.querySelector('button[type="submit"]');
+
+const onSusccess = ({ position, delay }) => {
+  Notiflix.Notify.success(
+    `✅ Fulfilled promise ${position} in ${delay}ms`
+  );
+};
+
+const onError = ({ position, delay }) => {
+  Notiflix.Notify.failure(
+    `❌ Rejected promise ${position} in ${delay}ms`
+  );
+};
+
+
+btnCreatePromise.addEventListener('click', e => {
+    e.preventDefault();
+    let firstDelay = Number(delay.value);
+    let delayStep = Number(step.value);
+    for (let i = 0; i < amount.value; i += 1) {
+      createPromise(1 + i, firstDelay + i * delayStep)
+        .then(onSusccess)
+        .catch(onError);
+    }
+  });
+
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  }); 
 }
